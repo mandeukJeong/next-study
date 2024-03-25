6b53adc5c3cd6b41877e217f23982183f404501c
mandeukJeong <wjdalstj137@gmail.com>
2024-03-25 23:42:53 +0900

    part1 - map

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,13 +1,14 @@
 export default function List() {
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut'];
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {상품.map((item) => (
+        <div className="food">
+          <h4>{item} $40</h4>
+        </div>
+      ))}
     </div>
   );
 }
