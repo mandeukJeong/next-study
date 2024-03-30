fcc3bc46a37a25361e532eda7b0050282069048e
mandeukJeong <wjdalstj137@gmail.com>
2024-03-30 22:28:14 +0900

    part1 - 이미지 넣기

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,3 +1,17 @@
 export default function Cart() {
-  return <h1>장바구니입니다.</h1>;
+  return (
+    <div>
+      <h4 className="title">Cart</h4>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+    </div>
+  );
 }
--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -4,8 +4,9 @@ export default function List() {
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      {상품.map((item) => (
-        <div className="food">
+      {상품.map((item, i) => (
+        <div className="food" key={i}>
+          <img src={`/food${i}.png`} className="food-img" />
           <h4>{item} $40</h4>
         </div>
       ))}
