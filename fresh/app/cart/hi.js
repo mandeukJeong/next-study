4de691f3f4e96816f8f8f948c4c1d7ab2752a988
mandeukJeong <wjdalstj137@gmail.com>
2024-04-01 22:11:30 +0900

    part1 - props

--- fresh/app/cart/hi.js
+++
@@ -1,3 +0,0 @@
-export default function Hi() {
-  return <h1>안녕하세요</h1>;
-}
--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,27 +1,19 @@
-import age from './data';
-import Hi from './hi';
-
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'Pasta'];
+
   return (
     <div>
-      <Hi />
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명 {age}</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      <CartItem item={장바구니[0]} />
+      <CartItem item={장바구니[1]} />
     </div>
   );
 }
 
-function CartItem() {
+function CartItem(props) {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{props.item}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
