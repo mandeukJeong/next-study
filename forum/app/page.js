b6b69898d1c2c7e1b78939a3f96d666bfd977679
mandeukJeong <wjdalstj137@gmail.com>
2024-04-09 01:23:27 +0900

    part2 - 소셜로그인

--- forum/app/page.js
+++ forum/app/page.js
@@ -3,7 +3,6 @@ import { connectDB } from '@/util/database';
 export default async function Home() {
   const db = (await connectDB).db('next');
   let result = await db.collection('post').find().toArray();
-  console.log(result);
 
   return <div>안녕</div>;
 }
