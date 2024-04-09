240a5eb4444a732f788c94e23cf5cb9e21fc12ef
mandeukJeong <wjdalstj137@gmail.com>
2024-04-09 16:32:46 +0900

    part2 - OAuth + session

--- forum/pages/api/post/new.js
+++ forum/pages/api/post/new.js
@@ -1,6 +1,14 @@
 import { connectDB } from '@/util/database';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handler(request, response) {
+  let session = await getServerSession(request, response, authOptions);
+
+  if (session) {
+    request.body.author = session.user.email;
+  }
+
   if (request.method === 'POST') {
     if (request.body.title === '' || request.body.content === '') {
       return response.status(400).json('모든 값을 입력해주세요');
@@ -11,6 +19,7 @@ export default async function handler(request, response) {
       let result = await db.collection('post').insertOne({
         title: request.body.title,
         content: request.body.content,
+        author: request.body.author,
       });
       return response.status(200).redirect('/list');
     } catch (error) {
